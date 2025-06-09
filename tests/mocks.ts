32554dab39618c9815f6c4904fdf7ffef4e709a4
Cesar Daniel <cesardaniel.9611@gmail.com>
2025-06-09 10:33:15 -0400

    refactor(tests): cleaned up tests setup files

--- tests/mocks.ts
+++ tests/mocks.ts
@@ -1,28 +1,5 @@
 import { setupServer } from "msw/node";
 import { openApiHandlers } from "./handlers";
-import { beforeAll, afterEach, afterAll } from "vitest";
+import { http, HttpResponse } from "msw";
 
 export const server = setupServer(...openApiHandlers);
-
-// Start server before all tests
-// beforeAll(() => {
-//   server.listen({
-//     onUnhandledRequest: (request) => {
-//       console.error("MSW Unhandled request:", request.method, request.url);
-//       // FIXME: Unhandled requests throw errors to fail tests fast, but this can mask
-//       // subtle handler mismatches (e.g., /account/:id). Consider logging warnings during
-//       // debugging to allow catch-all handlers to capture mismatches.
-//       throw new Error(`Unhandled ${request.method} request to ${request.url}`);
-//     },
-//   });
-// });
-//
-// // Reset handlers after each test to prevent interference
-// afterEach(() => {
-//   server.resetHandlers();
-// });
-//
-// // Close server after all tests
-// afterAll(() => {
-//   server.close();
-// });
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,13 @@
 import { beforeAll, afterEach, afterAll } from "vitest";
 import { server } from "./tests/mocks";
 
-beforeAll(() => server.listen());
+beforeAll(() =>
+  server.listen({
+    onUnhandledRequest: (request) => {
+      console.error("MSW Unhandled request:", request.method, request.url);
+      // throw new Error(`Unhandled ${request.method} request to ${request.url}`);
+    },
+  }),
+);
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
